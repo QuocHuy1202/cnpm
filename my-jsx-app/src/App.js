1e49fa125e7e45799b1367f3af61c1cf6ee82164
QuocHuy1202 <huy.huynh120204@hmail.com>
2024-11-29 17:28:29 +0700

    newupdate

--- my-jsx-app/src/App.js
+++ my-jsx-app/src/App.js
@@ -1,9 +1,11 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { TrangChu } from "./pages/index.jsx";  // Import component TrangCh
 import {Login} from "./pages/login.jsx"
+import { TaiFile } from "./pages/loadfile.jsx"
 import {PrintPage} from "./pages/printsetting.jsx"
+import {Print} from "./pages/print.jsx"
+import {Homen} from "./pages/home.jsx"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
@@ -12,9 +14,12 @@ function App() {
       <Router>
         
           <Routes>
-            <Route path="/" element={<TrangChu />} />
+            <Route path="/" element={<Homen />} />
             <Route path="/login" element={<Login />} />
             <Route path="/printsetting" element={<PrintPage />} />
+            <Route path="/loadfile" element={<TaiFile />} />
+            <Route path="/print" element={<Print />} />
+            
           </Routes>
       </Router>
     
